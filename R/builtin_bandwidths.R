2d8286fa59ac5e0f812ac02a5a7badf3845dfc68
JonasMoss <jonas.gjertsen@gmail.com>
2018-02-15 15:52:43 +0100

    :-1:  Reorganized documentation in order to pass R CMD CHECK.

--- R/builtin_bandwidths.R
+++ R/builtin_bandwidths.R
@@ -3,11 +3,33 @@
 #' Bandwidth selectors for \code{kdensity}. These are the available options
 #' for the option \code{bw} in \code{kdensity}.
 #'
+#' The bandwidth functions are not exported. They can be accessed by the
+#' kdensity:: prefix, e.g. kdensity::JE.
+#'
+#' @param x The input data.
+#' @param kernel_str A string specifying the kernel, e.g. "gaussian."
+#' @param start_str A string specifying the parametric start, e.g. "normal".
+#' @param support The domain of definiton for the kernel. (-Inf, Inf) for
+#' symmetric kernels.
+#'
 #' @section Bandwidth selectors:
 #'    \code{"nrd0", "nrd", "bcv", "SJ"}: Bandwidth selectors from \code{stats}.
 #'    They are documented in \code{\link[stats]{bandwidth} stats:bandwidth}.
 #'    "nrd0" is the standard bandwidth selector for symmetric kernels with
 #'    constant parametric starts.
+#'
+#'    \code{"ucv"}: Unbiased cross validation. The usual standard option for
+#'    asymmetric kernels.
+#'
+#'    \code{"RHE"}: Selector for parametric starts with a symmetric kernel,
+#'    based on a reference rule with Hermite polynomials.
+#'    Described in Hjort & Glad (1995). The default method in \code{kdensity} when a parametric
+#'    start is supplied and the kernel is symmetric.
+#'
+#'    \code{"JH"}: Selector for the Gaussian copula kernel, based on
+#'    normal reference rule. Described in Jones & Henderson. The default method when
+#'    the \code{gcopula} kernel is used in \code{kdensity}.
+#'
 #'
 #' @section Structure:
 #'    The bandwidth selector is a function of four arguments: The data
@@ -26,15 +48,14 @@
 #'      kernel = get_kernel(kernel)
 #'      kernel$kernel(0.5)
 #'    }
+#' @references
+#' Jones, M. C., and D. A. Henderson. "Kernel-type density estimation on the unit interval." Biometrika 94.4 (2007): 977-984.
+#' Hjort, Nils Lid, and Ingrid K. Glad. "Nonparametric density estimation with a parametric start." The Annals of Statistics (1995): 882-904.
 #' @name bandwidths
 
+
 #' @rdname bandwidths
-#' @usage NULL
-#' @section Bandwidth selectors:
-#'   \code{"JH"}: Selector for the Gaussian copula kernel, based on
-#'   normal reference rule. Described in Jones & Henderson. The default method when
-#'   the \code{gcopula} kernel is used in \code{kdensity}.
-#' @references Jones, M. C., and D. A. Henderson. "Kernel-type density estimation on the unit interval." Biometrika 94.4 (2007): 977-984.
+
 bw.JH = function(x, kernel = NULL, start = NULL, support = NULL) {
 
   # if(kernel != "gcopula") {
@@ -54,14 +75,6 @@ bw.JH = function(x, kernel = NULL, start = NULL, support = NULL) {
   min(sigma * (2 * mu^2 * sigma^2 + 3*(1 - sigma^2)^2)^(-1/5)*n^(-1/5), 0.5)
 }
 
-#' @rdname bandwidths
-#' @usage NULL
-#' @section Bandwidth selectors:
-#'   \code{"RHE"}: Selector for parametric starts with a symmetric kernel,
-#'   based on a reference rule with Hermite polynomials.
-#'   Described in Hjort & Glad (1995). The default method in \code{kdensity} when a parametric
-#'   start is supplied and the kernel is symmetric.
-#' @references Hjort, Nils Lid, and Ingrid K. Glad. "Nonparametric density estimation with a parametric start." The Annals of Statistics (1995): 882-904.
 bw.RHE = function(x, kernel = NULL, start = NULL, support = NULL) {
   assertthat::assert_that("EQL" %in% rownames(utils::installed.packages()), msg =
                             "The bandwidth function 'bw.RHE' requires the package 'EQL' to work.")
@@ -84,12 +97,6 @@ bw.RHE = function(x, kernel = NULL, start = NULL, support = NULL) {
     sigma * n^(-1/5)
   return(bw)
 }
-
-
-#' @rdname bandwidths
-#' @section Bandwidth selectors:
-#'   \code{"ucv"}: Unbiased cross validation. The usual standard option for
-#'   asymmetric kernels.
 
 bw.ucv = function(x, kernel = NULL, start = NULL, support = NULL) {
   kernel_obj      = get_kernel(kernel)
