d55b735e13de4fc552fd5593c56c15203bdfde21
JonasMoss <jonas.gjertsen@gmail.com>
2018-02-23 10:39:25 +0100

    :bug: Fixed recycle utility function.

--- R/utility_functions.R
+++ R/utility_functions.R
@@ -10,34 +10,47 @@
 #'
 #' @param ... A list of arguments to be recycled.
 #' @param prototype an optional argument. If given, repeats all arguments
-#' up to he length of the prototype. If an element of the list has the name,
-#' it is used. If not, the variable itself is used. If it is a non-negative
-#' integer,
+#' up to the length of the prototype. If an element of the list has the name,
+#' it is used. If not, the variable itself is used.
 #' @details Recycles arguments so that all vectors are equally long. If a
 #' prototype is given, each vector will have the same size as the prototype.
+#' @examples
+#'     a = 1:3
+#'     b = letters[2:9]
+#'     c = 9:20
+#'
+#'     # Returns a list where each element has length 5.
+#'     recycle(a, b, c, prototype = 5)
+#'
+#'     # Each element has the same length as c.
+#'     recycle(a, b, c, prototype = "c")
+#'     recycle(a, b, c, prototype = c)
 
 recycle = function(..., prototype) {
 
   dots = list(...)
-  arg_names = utils::tail(sapply(as.list(match.call()), function(x) as.character(x)), -1)
+  arg_names = utils::tail(sapply(as.list(match.call()),
+                                 function(x) as.character(x)), -1)
 
   if(missing(prototype)) {
     names(dots) = names(arg_names)
     max_length = max(sapply(dots, length))
   } else {
-    arg_names = utils::head(names(arg_names), -1)
+    arg_names = utils::head(arg_names, -1)
+    names(dots) = arg_names
 
     ## The rules work as follows: If it is a name, check the supplied list
     ## first. If itsn't there, use ordinary scoping to check. If it is a
     ## non-negative number, use it. If it's none of these, throw an error.
 
-    if(deparse(substitute(prototype)) %in% arg_names) {
-      max_length = length(dots[[substitute(prototype)]])
+    subst_proto = deparse(substitute(prototype))
+    if(subst_proto %in% arg_names) {
+      max_length = length(dots[[subst_proto]])
     } else if (length(prototype) > 1) {
         max_length = length(prototype)
     } else if (is.numeric(prototype)){
         if(prototype >= 0) max_length = ceiling(prototype)
-        else stop("supply a valid type. prototye is numeric and negative.")
+        else stop("supply a valid type. prototype is numeric and negative.")
     } else if (is.character(prototype)) {
         max_length = length(dots[[prototype]])
     } else {
