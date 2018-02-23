0b287d289866444d7eb736121c75c28868083305
Jonas Moss <jonas.gjertsen@gmail.com>
2018-02-23 17:24:04 +0100

    :notes: Increased coverage of assignments.

--- tests/testthat/test_generics.r
+++ tests/testthat/test_generics.r
@@ -4,3 +4,9 @@ obj = kdensity(mtcars$mpg, kernel = "gaussian", start = "gumbel")
 
 expect_equal(obj$h, obj[["h"]])
 expect_s3_class(logLik(obj), "logLik")
+
+expect_equal(update(obj, x = 1:100)(1:10),
+             kdensity(1:100, kernel = "gaussian", start = "gumbel")(1:10))
+
+
+
