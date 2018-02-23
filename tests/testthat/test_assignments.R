ff898c1439db6e63c7d1b175b96952e3bcb28e97
Jonas Moss <jonas.gjertsen@gmail.com>
2018-02-23 16:53:10 +0100

    :notes: Better coverage for assignments.

---
+++ tests/testthat/test_assignments.R
@@ -0,0 +1,16 @@
+context("assignments and updates")
+
+cars$dist %>%
+  kdensity(kernel = "gamma") ->
+  obj
+
+expect_equal(obj$start_str, "uniform")
+
+update(obj, start = "gamma")
+expect_equal(obj$start_str, "gamma")
+
+obj[["start"]] = "gaussian"
+expect_equal(obj$start_str, "gaussian")
+
+obj$start = "gumbel"
+expect_equal(obj$start_str, "gumbel")
