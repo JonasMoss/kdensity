7491e2c3e5ef9231b841b4e3d642e3cb63b6faef
JonasMoss <jonas.gjertsen@gmail.com>
2018-02-23 09:13:51 +0100

    :frog: Added first unit tests.

--- tests/testthat/test_starts.r
+++ tests/testthat/test_starts.r
@@ -1,6 +1,22 @@
-t = list(
+context("parametric starts")
+
+t_without_estim = list(
   density = dt,
-  estimator = NULL,
   support = c(-Inf, Inf)
 )
-add_start("t", t)
+
+t_without_support = list(
+  density = dt,
+  estimator = function(x) NULL
+)
+
+t_without_density= list(
+  estimator = function(x) NULL,
+  support = c(-Inf, Inf)
+)
+
+expect_error(add_start("t", t_without_estim))
+expect_error(add_start("t", t_without_support))
+expect_error(add_start("t", t_without_density))
+expect_error(get_start(5))
+expect_error(get_start("no_start"))
