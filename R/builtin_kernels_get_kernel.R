458adad8f71bcc1bb599aeae559f6b8da922cf6f
Jonas Moss <jonas.gjertsen@gmail.com>
2018-02-23 16:52:06 +0100

    :notes: Complete coverage for get_kernel.

--- R/builtin_kernels_get_kernel.R
+++ R/builtin_kernels_get_kernel.R
@@ -10,13 +10,8 @@ get_kernel = function(kernel_str) {
 
   kernel = kernel_environment[[kernel_str]]
 
-  if(is.null(kernel)) {
-    if(exists(kernel_str)) {
-      parametric_start = get(kernel_str)
-    } else {
-      stop(paste0("The supplied kernel ('", kernel_str,"') is not implemented."))
-    }
-  }
+  msg = paste0("The supplied kernel ('", kernel_str,"') is not implemented.")
+  assertthat::assert_that(!is.null(kernel), msg = msg)
 
   kernel
 }
--- tests/testthat/test_kernels.r
+++ tests/testthat/test_kernels.r
@@ -8,6 +8,13 @@ t_without_support = list(
   kernel = dt
 )
 
+t = list(
+  kernel = dt,
+  support = c(-Inf, Inf)
+)
+
+expect_equal(get_kernel("normal")$sd, 1)
+expect_silent(add_kernel("t", t))
 expect_error(add_kernel("t", t_without_kernel))
 expect_error(add_kernel("t", t_without_support))
 expect_error(get_kernel(5))
