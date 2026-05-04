ac533bae49d55737091dc0e37689c93bb28f0eb0
Jonas Moss <jonas.gjertsen@gmail.com>
2026-05-04 14:38:06 +0200

    Trim optional package dependencies

--- R/kdensity.R
+++ R/kdensity.R
@@ -97,27 +97,6 @@
 #' )
 #' rug(concentration)
 #'
-#' ## Using a density and and estimator from another package.
-#'
-#' skew_hyperbolic <- list(
-#'   density   = SkewHyperbolic::dskewhyp,
-#'   estimator = function(x) SkewHyperbolic::skewhypFit(x, printOut = FALSE)$param,
-#'   support   = c(-Inf, Inf)
-#' )
-#'
-#' kde <- kdensity(diff(LakeHuron), start = skew_hyperbolic)
-#' plot(kde,
-#'   lwd = 2, col = "blue",
-#'   main = "Annual differences in water level (ft) of Lake Huron, 1875 - 1972"
-#' )
-#' lines(kde, plot_start = TRUE, lty = 2, lwd = 2) # Plots the skew hyperbolic density.
-#' rug(diff(LakeHuron))
-#'
-#' kde$estimates # Also: coef(kde)
-#' # Displays the parameter estimates:
-#' #        mu     delta      beta        nu
-#' # -1.140713  3.301112  2.551657 26.462469
-#'
 kdensity <- function(x, bw = NULL, adjust = 1, kernel = NULL, start = NULL,
                      support = NULL, na.rm = FALSE, normalized = TRUE,
                      tolerance = 0.01) {
